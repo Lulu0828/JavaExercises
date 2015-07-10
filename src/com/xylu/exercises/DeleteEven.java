0106e9eaaaab24f86b12dbf750227032f0c0f818
Lulu0828 <230760628@qq.com>
2015-07-10 13:54:50 +0800

    增加说明

--- src/com/xylu/exercises/DeleteEven.java
+++ src/com/xylu/exercises/DeleteEven.java
@@ -3,6 +3,8 @@ package com.xylu.exercises;
 import java.util.ArrayList;
 import java.util.List;
 
+//list存放数值类型数据，数据为1,2,3,4,5。要求：剔除偶数数据
+
 public class DeleteEven {
 
 	public static void main(String[] args) {
